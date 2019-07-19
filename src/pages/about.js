043dd10dc7fc8df8d7aba5477d0d973cfd33fd30
Loren Mariquit <lmariquit@dotdash.com>
2019-07-19 13:10:17 -0400

    working site. Added Layout tags to all pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Layout from "../components/layout"
 
 const AboutPage = () => (
-	<div>
-		<h1>About Us</h1>			
-		<p>I am Loren</p>
-	</div>
+	<Layout>
+		<div>
+			<h1>About Us</h1>			
+			<p>I am Loren</p>
+		</div>
+	</Layout>
 )
 
 export default AboutPage
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import Layout from "../components/layout"
 
 const ServicesPage = () => (
-	<div>
-		<h1>Our Services</h1>			
-		<p>Service 1</p>
-		<p>Service 2</p>
-		<p>Service 3</p>
-	</div>
+	<Layout>
+		<div>
+			<h1>Our Services</h1>			
+			<p>Service 1</p>
+			<p>Service 2</p>
+			<p>Service 3</p>
+		</div>
+	</Layout>
 )
 
 export default ServicesPage
