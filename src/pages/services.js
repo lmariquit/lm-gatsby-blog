12d6ddf51e7022eaa5fd908d3a86a3c829cbd1da
Loren Mariquit <lmariquit@dotdash.com>
2019-07-19 12:02:41 -0400

    created about and services page. in middle of menu page

---
+++ src/pages/services.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ServicesPage = () => {
+	return (
+		<div>
+			<h1>Our Services</h1>			
+			<p>Service 1</p>
+			<p>Service 2</p>
+			<p>Service 3</p>
+		</div>
+	)
+}
+
+export default ServicesPage
